f30531797dbcfcdd90be4ea99ae1f692746dbdb1
Muhammad Hamza Ishaq <144256929+hafiz22e@users.noreply.github.com>
2026-05-12 17:37:11 +0500

    Update task2.m

--- task2.m
+++ task2.m
@@ -1,5 +1,5 @@
 
-%%%%%%     HW 1 Machion Vision (EEE-584)      
+%%%%%%     HW 1 Machion Vision    
 %%%%%%     Task 2 Histrogram
 input=imread('C:\Users\DELL\Downloads\Compressed\Images\Image_1.jpg');%read image from directory
 figure(1)
